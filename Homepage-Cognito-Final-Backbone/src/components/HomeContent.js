3577beb61a5df9e2665e5db7b8daaa9141990f69
Arya <aryakoukia@gmail.com>
2020-04-30 18:15:56 -0700

    Added "home" content, these are templates for the home page

---
+++ Homepage-Cognito-Final-Backbone/src/components/HomeContent.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function HomeContent() {
+  return (
+    <section className="container">
+        <div className="columns features">
+            <div className="column is-4">
+                <div className="card is-shady">
+                    <div className="card-content">
+                        <div className="content">
+                            <h4>Notes</h4>
+                            <p>Note goes here, with edit/add</p>
+                            <p><a href="/">Learn more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="column is-4">
+                <div className="card is-shady">
+                    <div className="card-content">
+                        <div className="content">
+                        <h4>Notes</h4>
+                            <p>Note goes here, with edit/add</p>
+                          
+                            <p><a href="/">Learn more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="column is-4">
+                <div className="card is-shady">
+                     <div className="card-content">
+                        <div className="content">
+                        <h4>Notes</h4>
+                            <p>Note goes here, with edit/add</p>
+                            <p><a href="/">Learn more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
