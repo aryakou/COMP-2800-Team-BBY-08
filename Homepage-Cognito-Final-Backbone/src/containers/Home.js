38b5d4c673eb832c4038ad9512d6da72d9643dbc
Arya K <agolbaghikoukia@my.bcit.ca>
2020-05-06 17:34:40 -0700

    Fixed home screen image, changed home screen text

--- Homepage-Cognito-Final-Backbone/src/containers/Home.js
+++ Homepage-Cognito-Final-Backbone/src/containers/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="lander">
         <img src='https://image.freepik.com/free-vector/closeup-fountain-pen-writing-signature-realistic_1284-13522.jpg' alt='logo' />
         <h1>Green Notes</h1>
-        <p>We in Green Notes bilieve that all deserve to have access to a note taking software which will allow them to organize and navigate during theese trying times. We all will work together and build such a software , That is our team culture! </p>
+        <p>Here at GreenNotes we believe in building an efficient, easy to use and fun application for our users during this unprecendented time. Create your notes, stay on task, and get through quarantine with ease.</p>
       </div>
     </div>
   );
