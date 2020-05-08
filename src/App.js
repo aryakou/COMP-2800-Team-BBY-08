3923af5f4c17f92d94727687c5a691f3602c4ce1
Gurjot Sandher <g_sandher@hotmail.com>
2020-05-07 21:51:20 -0700

    Merge remote-tracking branch 'origin/sandher-client-side'

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import { LinkContainer } from "react-router-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Nav, Navbar, NavItem } from "react-bootstrap";
+import "./App.css";
+import Routes from "./Routes";
+import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
+import { onError } from "./libs/errorLib";
+
+
+
+function App() {
+  const history = useHistory();
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+  
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+  
+    userHasAuthenticated(false);
+
+    history.push("/login");
+  }
+  return (
+    !isAuthenticating && (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">MML</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {isAuthenticated ? (
+                <>
+                  <LinkContainer to="/settings">
+                    <NavItem>settings</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/about">
+                    <NavItem>about us</NavItem>
+                  </LinkContainer>
+                  <NavItem onClick={handleLogout}>logout</NavItem>
+                </>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <NavItem>sign up</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem>login</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/about">
+                    <NavItem>about us</NavItem>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider
+          value={{ isAuthenticated, userHasAuthenticated }}
+        >
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
+  );
+}
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,20 @@
+export default {
+
+  STRIPE_KEY: "pk_test_NgblqJUC8nqHVtPhxvOmDRRq00aOei7J8S",
+  MAX_ATTACHMENT_SIZE: 5000000,
+
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "bcit.group8"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://gwrmtps6ib.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_GrsmgRaQb",
+    APP_CLIENT_ID: "lrmkh1urss844vn3s5vmpqqb4",
+    IDENTITY_POOL_ID: "us-west-2:25c47f40-9fc4-4860-b7a9-03e44147329f"
+  }
+};
